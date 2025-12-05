ebac731523000f88c80d37bc6916b75b3e95324b
Jayfablead <jay.fablead@gmail.com>
2025-12-05 16:26:54 +0530

    05-12-2025

--- lib/Utils/const.dart
+++ lib/Utils/const.dart
@@ -116,10 +116,10 @@ String formatTime12(String? time) {
 }
 
 class AppConstants {
-  static String BASEURL = "https://development.wavee.ai/api/";
+  // static String BASEURL = "https://development.wavee.ai/api/";
 
   /// Og Url
-  // static String BASEURL = "https://portal.wavee.ai/api/";
+  static String BASEURL = "https://portal.wavee.ai/api/";
 
   static const String path = "assets/Svg/";
   static const String manrope1 = "AlbertSans-SemiBold";
